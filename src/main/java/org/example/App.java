723bd7d5b1f1605077e93f6fd4523125d3a2b37b
omar derbak <omar.derbak93@gmail.com>
2023-07-07 08:56:15 +0200

    remove spaces

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,47 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        String op;
+
+        Scanner numbers = new Scanner(System.in);
+
+        System.out.println("First number");
+        a = numbers.nextInt();
+
+        System.out.println("Second number");
+        b = numbers.nextInt();
+
+        System.out.println("Third number");
+        c = numbers.nextInt();
+
+        Scanner operation = new Scanner(System.in);
+        System.out.println("enter operation");
+        op = operation.next();
+
+        switch (op) {
+            case "+":
+                System.out.println(a + " + " + b + " + " + c + " = " + (a + b + c));
+                break;
+            case "-":
+                System.out.println(a + " - " + b + " - " + c + " = " + (a - b - c));
+                break;
+            case "*":
+                System.out.println(a + " * " + b + " * " + c + " = " + (a * b * c));
+                break;
+            case "/":
+                System.out.println(a + " / " + b + " / " + c + " = " + (a / b / c));
+
+        }
+
+
     }
 }
